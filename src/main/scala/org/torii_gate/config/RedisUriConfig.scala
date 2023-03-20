649db826e3c9974829c66aa89092e1ea2bf8952f
Ibrahim Moreno <kvltd00d@gmail.com>
2023-03-19 21:40:46 -0400

    redis instance now top-level + Changed RedisUriConfig structure

--- src/main/scala/org/torii_gate/config/RedisUriConfig.scala
+++ src/main/scala/org/torii_gate/config/RedisUriConfig.scala
@@ -5,7 +5,7 @@ import zio.config.*
 import zio.config.magnolia.descriptor
 import zio.config.typesafe.TypesafeConfigSource
 
-case class RedisUriConfig(uri: String, port: Int)
+case class RedisUriConfig(uri: String, host: String, username: String, password: String, port: Int)
 
 object RedisUriConfig {
   val live: ZLayer[Any, ReadError[String], RedisUriConfig] =
